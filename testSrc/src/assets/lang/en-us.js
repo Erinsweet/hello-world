a47ba35e0f04fb19b68fc5c121f79e44f51d42c5
Erinsweete <1983179332u@qq.com>
2021-01-28 00:50:04 +0800

    add file

---
+++ testSrc/src/assets/lang/en-us.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 江苏立讯机器人有限公司. [ http://www.luxshare-ict.com ]
+ * User: apeol
+ * Date: 2018/10/12
+ * Time: 21:16
+ */
+module.exports = {
+  userName : 'user name',
+  message : 'Click on the button below to many language switching',
+  status: 'normal'
+}
---
+++ testSrc/src/assets/lang/zh-cn.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 江苏立讯机器人有限公司. [ http://www.luxshare-ict.com ]
+ * User: apeol
+ * Date: 2018/10/12
+ * Time: 21:16
+ */
+module.exports = {
+  userName : '用户名',
+  message : '点击下面的按钮可以进行多语言切换',
+  status: '正常'
+}
