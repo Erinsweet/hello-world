bb9383db0f27680b1fd10ad8c763cfd81cb847e2
Erinsweete <1983179332u@qq.com>
2021-03-04 08:36:12 +0800

    q

--- testSrc/src/mock.js
+++ testSrc/src/mock.js
@@ -3,12 +3,36 @@
  *DateTime: 2021/1/27 11:46 下午
  */
 import Vue from "vue";
-console.log(88888,Vue);
  // Vue.prototype.$common;
 export default {
-
     testData:{
        // name1:this.$t('status')
     }
 }
 
+// <div class="flag">
+//     <span>进行中</span>
+//     </div>
+//     .flag{
+//     position: absolute;
+//     top:-.77rem;
+//     right: -.28rem;
+//     width: 1.7rem;
+//     height: 1.7rem;
+//     overflow: hidden;
+//     text-align: center;
+//     background-color: #07c68a;
+// .rotate(45deg, 100%, 100%);
+//     box-sizing: border-box;
+//     padding-top: 1.4rem;
+//     color: @lightFontColor;
+// }
+
+//     .rotate(@val:90deg, @x:0, @y:0){
+// .origin(@x @y);
+//     transform:rotate(@val);
+//     -ms-transform:rotate(@val);
+//     -moz-transform:rotate(@val);
+//     -webkit-transform:rotate(@val);
+//     -o-transform:rotate(@val);
+// }
