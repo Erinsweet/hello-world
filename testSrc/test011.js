ddf5a9be016be5c9059619d072f5484066df4f9a
Erinsweete <1983179332u@qq.com>
2021-05-29 22:17:10 +0800

    1

---
+++ testSrc/test011.js
@@ -0,0 +1,6 @@
+/*
+  1: "https://blog.csdn.net/ayumu5566/article/details/79384546?locationNum=15&fps=1",
+     nowPdfData = pdf.output('datauristring')
+  3.https://blog.csdn.net/weixin_43672560/article/details/105121786 接收file obj
+  var base64 = datauri.substring(28);
+ */
