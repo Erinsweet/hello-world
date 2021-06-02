a9295b955c1eed348d491c3f4f47160c5a1352de
Erinsweete <1983179332u@qq.com>
2021-06-02 08:40:22 +0800

    1

--- testSrc/test011.js
+++ testSrc/test011.js
@@ -3,4 +3,7 @@
      nowPdfData = pdf.output('datauristring')
   3.https://blog.csdn.net/weixin_43672560/article/details/105121786 接收file obj
   var base64 = datauri.substring(28);
+
+
+  https://blog.csdn.net/weixin_45295262/article/details/117041018
  */
